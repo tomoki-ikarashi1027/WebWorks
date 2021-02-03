d01719b097c64cac86e082cddd5c28a9ebe074fd
tomoki <lb.s0172@gmail.com>
2021-02-04 01:39:16 +0900

    [Fix]コード整形

--- app/controllers/communities_controller.rb
+++ app/controllers/communities_controller.rb
@@ -2,7 +2,7 @@ class CommunitiesController < ApplicationController
   before_action :authenticate_user!
   def index
     @communities = Community.all.order('created_at DESC')
-    @users = User.where.not(id: current_user.id)
+    @users = User.where.not(id: current_user.id).order('created_at DESC')
     @community = Community.new
 
   end
